32ed0996994da270ff8948636eb5d5d2b742ff8c
DanyRanjbar <danyranjbar@gmail.com>
2023-02-26 12:18:51 +0330

    third commit

---
+++ components/Header/page.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <header>
+            <div className=" flex justify-between items-center">
+                <span>
+
+                </span>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ components/navButton/page.jsx
@@ -0,0 +1,12 @@
+const NavButton = () => {
+    return (
+        <div>
+            <button className=" w-20 h-12 text-lg bg-transparent flex justify-center items-center">Home</button>
+            <button className=" w-20 h-12 text-lg bg-transparent flex justify-center items-center">Home</button>
+            <button className=" w-20 h-12 text-lg bg-transparent flex justify-center items-center">Home</button>
+            <button className=" w-20 h-12 text-lg bg-transparent flex justify-center items-center">Home</button>
+        </div>
+    );
+}
+
+export default NavButton
